60483d1d95177536c90728021a2432504508f40e
Brian Preskitt <bppresk@outlook.com>
2020-04-18 22:41:11 -0700

    Add script to take diffs of dots.  It's called diff-dots.

---
+++ diff-dots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SRC_DIR="$(dirname ${BASH_SOURCE[0]})"
+declare -A dotfiles
+dotfiles=(
+  ["$SRC_DIR/zshrc"]="$HOME/.zshrc"
+  ["$SRC_DIR/zsh_aliases"]="$HOME/.zsh_aliases"
+  ["$SRC_DIR/zshrc_pure"]="$HOME/.zshrc_pure"
+  ["$SRC_DIR/emacs.d/init.el"]="$HOME/.emacs.d/init.el"
+  ["$SRC_DIR/emacs.d/lisp/mycode.el"]="$HOME/.emacs.d/lisp/mycode.el"
+  ["$SRC_DIR/emacs.d/lisp/keybindings.el"]="$HOME/.emacs.d/lisp/keybindings.el"
+)
+
+for src in "${!dotfiles[@]}"; do
+  diff_command="diff -s $src ${dotfiles[$src]}"
+  echo $diff_command
+  eval $diff_command
+  echo "========================================================="
+  echo ""
+done
