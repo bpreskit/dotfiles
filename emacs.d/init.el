35a6eb199803c7377f69faa8d6b02ef3de4043ed
Brian Preskitt <bpreskitt@purestorage.com>
2020-04-26 03:01:09 +0000

    Use emacsclient for git and add org-bullets.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,7 +31,7 @@
  '(org-todo-keywords (quote ((sequence "TODO(t)" "NEXT(n)" "DONE(d)"))))
  '(package-selected-packages
    (quote
-    (nginx-mode apache-mode all-the-icons-ivy all-the-icons-dired all-the-icons org markdown-mode w3m load-theme-buffer-local ansible-doc yaml-mode golden-ratio sr-speedbar ivy go-guru counsel-etags ansible rtags elpy flycheck company neotree flymake-go go-autocomplete tern-auto-complete tern go-complete jedi company-ycmd flycheck-ycmd ycmd rjsx-mode jsx-mode magit dash smartparens multi-term mo-git-blame go-mode go-playground electric-case projectile)))
+    (nginx-mode apache-mode all-the-icons-ivy all-the-icons-dired all-the-icons org org-bullets markdown-mode w3m load-theme-buffer-local ansible-doc yaml-mode golden-ratio sr-speedbar ivy go-guru counsel-etags ansible rtags elpy flycheck company neotree flymake-go go-autocomplete tern-auto-complete tern go-complete jedi company-ycmd flycheck-ycmd ycmd rjsx-mode jsx-mode magit dash smartparens multi-term mo-git-blame go-mode go-playground electric-case projectile)))
  '(projectile-completion-system (quote ivy))
  '(sh-basic-offset 2)
  '(shell-prompt-pattern "^[^#$%>
