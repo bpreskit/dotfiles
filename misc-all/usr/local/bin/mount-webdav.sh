ebf5c7f131d2e70920e251be05002e70c85a0085
Brian Preskitt <bpreskitt@purestorage.com>
2021-11-10 20:11:10 +0000

    whitespace, new alias, tmux-proofing mount-webdav.

--- misc-all/usr/local/bin/mount-webdav.sh
+++ misc-all/usr/local/bin/mount-webdav.sh
@@ -2,4 +2,7 @@
 
 mkdir /tmp/webdav
 sudo mount /tmp/webdav
+# With tmux, it's possible that users will return a repeated list
+# e.g., "bpreskitt bpreskitt" rather than "bpreskitt".
+user=$(users | cut -d ' ' -f 1)
 sudo chown -R $(users):$(users) /tmp/webdav
