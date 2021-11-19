0eb42ffb98c78a4050067733cabdb3b6a004e445
Brian Preskitt <bppresk@outlook.com>
2021-11-18 20:34:47 -0800

    open ods in libre

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -35,7 +35,8 @@
      ("\\.\\(?:mpe?g\\|avi\\|wmv\\)\\'" "mplayer"
       ("-idx" file))
      ("\\.\\(?:jp?g\\|png\\)\\'" "xviewer"
-      (file)))))
+      (file))
+     ("\\.ods\\'" "libreoffice" (file)))))
  '(package-selected-packages
    (quote
     (json-mode groovy-mode tide openwith ox-slimhtml nginx-mode apache-mode all-the-icons-ivy all-the-icons-dired all-the-icons org org-bullets markdown-mode w3m load-theme-buffer-local ansible-doc yaml-mode golden-ratio sr-speedbar ivy go-guru counsel-etags ansible rtags elpy flycheck company neotree flymake-go go-autocomplete tern-auto-complete tern go-complete jedi company-ycmd flycheck-ycmd ycmd rjsx-mode jsx-mode magit dash smartparens multi-term mo-git-blame go-mode go-playground electric-case projectile)))
