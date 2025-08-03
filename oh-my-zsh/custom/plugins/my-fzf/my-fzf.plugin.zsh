762efd5f882b2efafb80be37ed13df1b65570d58
Brian Preskitt <bppresk@outlook.com>
2025-08-02 21:49:23 -0700

    Add some --multi to fzf by default

--- oh-my-zsh/custom/plugins/my-fzf/my-fzf.plugin.zsh
+++ oh-my-zsh/custom/plugins/my-fzf/my-fzf.plugin.zsh
@@ -1,17 +1,18 @@
-export FZF_DEFAULT_OPTS='--bind "alt-v:page-up,ctrl-v:page-down,alt-<:last,alt->:first"'
+export FZF_DEFAULT_OPTS='--multi --bind "alt-v:page-up,ctrl-v:page-down,alt-<:last,alt->:first,alt-c:select-all+accept"'
 export MY_FZF_EMACS_WINDOW=${MY_FZF_EMACS_WINDOW:-"GNU Emacs"}
 
 function fzg {
     local rg_options=$(printf "%q" "$@")
     local reload="reload:rg ${rg_options} --line-number --column --color=always --smart-case {q} || :"
     fzf --disabled --ansi \
-              --bind "start:$reload" \
-              --bind "change:$reload" \
-              --delimiter : \
-              --bind "alt-l:become(batcat --pager='less +{2}' --style=numbers --color=always --highlight-line {2} {1})" \
-              --bind "alt-e:become(emacsclient -n +{2} {1}; wmctrl -a '${MY_FZF_EMACS_WINDOW}')" \
-              --preview 'batcat --style=numbers --color=always --highlight-line {2} {1}' \
-              --preview-window '+{2}/2'
+        --no-multi \
+        --bind "start:$reload" \
+        --bind "change:$reload" \
+        --delimiter : \
+        --bind "alt-l:become(batcat --pager='less +{2}' --style=numbers --color=always --highlight-line {2} {1})" \
+        --bind "alt-e:become(emacsclient -n +{2} {1}; wmctrl -a '${MY_FZF_EMACS_WINDOW}')" \
+        --preview 'batcat --style=numbers --color=always --highlight-line {2} {1}' \
+        --preview-window '+{2}/2'
 }
 
 function gbg {
@@ -20,9 +21,11 @@ function gbg {
 }
 
 function gcog {
-  git branch | fzf --preview 'git show --color=always {-1}' \
-                   --bind 'enter:become(git checkout {-1})' \
-                   --height 40% --layout reverse
+    git branch | fzf --no-multi \
+                     --preview 'git show --color=always {-1}' \
+                     --bind 'enter:become(git checkout {-1})' \
+                     --height 40% \
+                     --layout reverse
 }
 
 function fzfd {
