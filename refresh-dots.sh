6f37127794999ed531de3aef97c8e64cb4602d6d
Brian Preskitt <bppresk@outlook.com>
2025-08-03 01:37:59 -0700

    refresh-dots only looks at dotfiles, no ansible, always ediff

--- refresh-dots.sh
+++ refresh-dots.sh
@@ -4,37 +4,40 @@
 usage() {
   cat <<EOF
 refresh-dots.sh [--help] [--no-ansible] [--no-emacs] [--ansible-copy]
-  --no-ansible
-    Completely skip the ansible playbook, which would usually tell you which
-    zsh files differ between your machine and the repo (plus some git config
-    settings).
+  --no-other
+    Skip non-emacs dot files.
   --no-emacs
     Skip files in ~/.emacs.d
-  --ansible-copy
-    Live run of the ansible -- don't worry, your .zsh*'s will be backed up.
   -h, --help
     Show this help.
 EOF
   exit 1
 }
 
-no_ansible=""
-ansible_args="--check"
+# Keep this in sync with copy_items in unpack_dots/vars/main.yml
+OTHER_DOTS=(
+  zshrc
+  zsh_aliases
+  gitignore_global
+  tmux.conf
+)
+OTHER_DOT_DIRS=(
+  oh-my-zsh
+  config
+)
+
+no_other=""
 no_emacs=""
 
 parse_args() {
   while [[ -n "$1" ]]; do
     case "$1" in
-      "--no-ansible")
-        no_ansible=1
-        shift
-        ;;
       "--no-emacs")
         no_emacs=1
         shift
         ;;
-      "--ansible-copy")
-        ansible_args=""
+      "--no-other")
+        no_other=1
         shift
         ;;
       *)
@@ -44,48 +47,84 @@ parse_args() {
   done
 }
 
+maybe_ediff_files () {
+    local local_file=$1
+    local repo_file=$2
+    if [[ -f "${local_file}" ]]; then
+        if ! diff -q "${repo_file}" "${local_file}" &>/dev/null; then
+            read -p "${repo_file} differs between repo and local. Ediff them? (y/N) " -N 1 resp
+            if grep -qPi "^y$" <<<$resp; then
+                ediff_cmd="(ediff \"${repo_file}\" \"${local_file}\")"
+                ALTERNATE_EDITOR="emacs" emacsclient --eval "$ediff_cmd"
+            else
+                echo "Skipping ediff of ${repo_file} and ${local_file}."
+            fi
+        else
+            echo "No difference between ${repo_file} and ${local_file}. Skipping."
+        fi
+    else
+        echo "local didn't exist"
+        read -p "${repo_file} doesn't exist locally. Copy it in? (y/N) " -N 1 resp
+        if grep -qPi "^y$" <<<$resp; then
+            cp "${repo_file}" "${local_file}"
+        fi
+    fi
+}
+
+do_emacs () {
+    pushd "${repo_dir}/emacs.d"
+
+    local file_array=()
+    local file
+    local repo_file
+    local local_file
+    local base_emacs_dir
+    while read file; do
+        file_array=("${file_array[@]}" "$file")
+    done < <(find . -type f -name "*.el")
+
+    base_emacs_dir="${HOME}/.emacs.d"
+    for repo_file in "${file_array[@]}"; do
+        local_file="${base_emacs_dir}/${repo_file#./}"
+        maybe_ediff_files ${local_file} ${repo_file}
+    done
+
+    popd
+}
+
+do_other () {
+    pushd "${repo_dir}"
+    local repo_file
+    local local_file
+    for repo_file in ${OTHER_DOTS}; do
+        local_file="${HOME}/.${repo_file}"
+        maybe_ediff_files "${local_file}" "${repo_file}"
+    done
+    local file_array=()
+    local file
+
+    for repo_file_dir in ${OTHER_DOT_DIRS}; do
+        file_array=()
+        while read file; do
+            file_array=("${file_array[@]}" "$file")
+        done < <(fd --type file . "${repo_file_dir}")
+    done
+    for repo_file in "${file_array[@]}"; do
+        local_file="${HOME}/.${repo_file}"
+        maybe_ediff_files ${local_file} ${repo_file}
+    done
+    popd
+}
+
 parse_args "$@"
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 repo_dir=$(git rev-parse --show-toplevel)
 
-if [[ -z "${no_ansible}" ]]; then
-  pushd "${script_dir}/ansible-setup"
-  ansible-playbook ${ansible_args} -i local-machine refresh-dots.yml
-  popd
+if [[ -z "${no_other}" ]]; then
+    do_other
 fi
 
 if [[ -z "${no_emacs}" ]]; then
-  pushd "${repo_dir}/emacs.d"
-
-  file_array=()
-  while read file; do
-    file_array=("${file_array[@]}" "$file")
-  done < <(find . -type f -name "*.el")
-
-  base_emacs_dir="${HOME}/.emacs.d"
-  for repo_file in "${file_array[@]}"; do
-    local_file="${base_emacs_dir}/${repo_file#./}"
-    if [[ -f "${local_file}" ]]; then
-      if ! diff -q "${repo_file}" "${local_file}" &>/dev/null; then
-        read -p "${repo_file} differs between repo and local. Ediff them? (y/N) " -N 1 resp
-        if grep -qPi "^y$" <<<$resp; then
-          ediff_cmd="(ediff \"${repo_file}\" \"${local_file}\")"
-          ALTERNATE_EDITOR="emacs" emacsclient --eval "$ediff_cmd"
-        else
-          echo "Skipping ediff of ${repo_file} and ${local_file}."
-        fi
-      else
-        echo "No difference between ${repo_file} and ${local_file}. Skipping."
-      fi
-    else
-      echo "local didn't exist"
-      read -p "${repo_file} doesn't exist locally. Copy it in? (y/N) " -N 1 resp
-      if grep -qPi "^y$" <<<$resp; then
-        cp "${repo_file}" "${local_file}"
-      fi
-    fi
-  done
-
-  popd
+    do_emacs
 fi
