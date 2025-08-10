67132897aeff80948e8eff29155f7a32a6fe6a62
Brian Preskitt <bppresk@outlook.com>
2025-08-10 15:28:39 -0700

    Prioritize stable melpa

--- emacs.d/lisp/install-packages.el
+++ emacs.d/lisp/install-packages.el
@@ -1,9 +1,12 @@
 ;; Setup package archives.  Install anything that is missing.
 (require 'package)
 (add-to-list 'package-archives
-       '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+       '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archive-priorities '(("melpa-stable" . 10)
+                                   ("melpa" . 5)
+                                   ("gnu" . 0)))
 ;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (if (not
      (let ((install-result (package-install-selected-packages t)))
